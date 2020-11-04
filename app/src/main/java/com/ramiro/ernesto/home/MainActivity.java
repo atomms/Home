394f7c39ea1f23cb09c84a121677ad901bc2195c
Ernesto Ramiro <atomms@gmail.com>
2020-11-04 11:15:07 +0100

    initial

---
+++ app/src/main/java/com/ramiro/ernesto/home/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ramiro.ernesto.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author ernesto
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
