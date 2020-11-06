67299863fc0ddc010d491e09caece9b2ff62d361
Ernesto Ramiro <atomms@gmail.com>
2020-11-06 11:17:13 +0100

    añadir username password y button

---
+++ app/src/main/java/com/ramiro/ernesto/home/Constraining.java
@@ -0,0 +1,14 @@
+package com.ramiro.ernesto.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Constraining extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraining);
+    }
+}
